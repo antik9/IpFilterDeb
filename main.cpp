42149763c3651d5c8c61c3063bdf5a5333d4c259
antik9 <illarionovanton@gmail.com>
2018-07-08 04:10:40 +0300

    Fix bug with AND

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[])
 {
     try
     {
-        if (argc == 2 and std::string(argv[1]) == "-v" 
+        if (argc == 2 && std::string(argv[1]) == "-v" 
                 || std::string(argv[1]) == "--version") {
             std::cout << "VERSION: " << version() << std::endl;
             return 0;

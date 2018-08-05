ffe88536a308678027c8a262b98efb72ce2fb563
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-05 05:45:30 +0000

    Deploy antik9/OtusCpp to github.com/antik9/OtusCpp.git:gh-pages

---
+++ googletest-master/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/travis/build/antik9/OtusCpp/googletest-master/googlemock/src/gmock-all.cc" "/home/travis/build/antik9/OtusCpp/googletest-master/googlemock/CMakeFiles/gmock.dir/src/gmock-all.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GTEST_CREATE_SHARED_LIBRARY=1"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/travis/build/antik9/OtusCpp/googletest-master/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "."
+  "/home/travis/build/antik9/OtusCpp/googletest-master/googletest/include"
+  "/home/travis/build/antik9/OtusCpp/googletest-master/googletest"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/travis/build/antik9/OtusCpp/tests
+# Build directory: /home/travis/build/antik9/OtusCpp/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(print_ip_test "print_ip_test")

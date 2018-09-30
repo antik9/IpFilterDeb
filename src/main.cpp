6823b4c861c19fe786f600750823db6eb6b31807
antik9 <illarionovanton@gmail.com>
2018-09-30 18:14:26 +0300

    Add cleanup for matrix.

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@ int
 main(int args, char *argv[])
 {
     Matrix<int, 0> matrix_nines;
-    
+
     for ( int i = 0; i < 10; ++i )
     {
         matrix_nines[i][i] = i;
         matrix_nines[9 - i][i] = i;
     }
-    
+
     assert(matrix_nines.size() == 18);
-    
+
     for ( int i = 1; i <= 8; ++i )
     {
         for ( int j = 1; j <= 8; ++j )
@@ -26,7 +26,7 @@ main(int args, char *argv[])
     }
 
     assert(matrix_nines.size() == 18);
-   
+
     std::cout << matrix_nines.size() << std::endl;
 
     for ( auto tuple_v: matrix_nines )
@@ -38,7 +38,7 @@ main(int args, char *argv[])
     }
 
     assert(matrix_nines.size() == 18);
-   
+
     // Uncomment to try
     /*
     Matrix<long, -1, 3> volume;
@@ -52,12 +52,12 @@ main(int args, char *argv[])
         std::tie(x, y, z, value) = tuple_v;
         std::cout << x << y << z << value << std::endl;
     }
-    
+
     Matrix<char, 'a', 5> alpha;
     alpha[1][10][32][19990][123123] = 'b';
     alpha[9123][1453][2131][4][989] = 'z';
     alpha[9123][1453][2131][4][989] = 'w';
-    
+
     for ( auto tuple_v: alpha )
     {
         int x, y, z, x_, y_;
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -4,13 +4,6 @@
 #include <tuple>
 #include <utility>
 
-template <typename T>
-const int
-__stub_value(T)
-{
-    return -1;
-}
-
 /*
  * Struct which wrap std::map for all operations. Neeeded to obtain default value operations.
  */
@@ -19,23 +12,56 @@ struct MapWrapper
 {
 public:
 
+    void clean ( )
+    {
+        auto it = __values.find ( last_accessed_key );
+        if ( it != __values.end ( ) )
+        {
+            it->second.clean ( );
+            if ( it->second.size ( ) == 0 )
+            {
+                __values.erase ( it );
+            }
+        }
+    }
+
+    size_t
+    size ( ) { return __values.size ( ); }
+
     Layer& operator[] (const int& key)
     {
+        last_accessed_key = key;
         return __values[key];
     }
-    
+
     using iterator = decltype(std::map<int, Layer>().cbegin());
     iterator begin() const { return this->__values.cbegin(); }
     iterator end() const { return this->__values.cend(); }
 
 private:
-    std::map<int, Layer> __values;
+    std::map<int, Layer>    __values;
+    int                     last_accessed_key;
 };
 
 template <typename T, T default_v>
 struct MapWrapper<T, T, default_v>
 {
 public:
+
+    void clean ( )
+    {
+        auto it = __values.find ( last_accessed_key );
+        if ( it != __values.end ( ) )
+        {
+            if ( it->second == default_v )
+            {
+                __values.erase ( it );
+            }
+        }
+    }
+
+    size_t
+    size ( ) { return __values.size ( ); }
 
     T&
     operator[] (const int& key)
@@ -44,15 +70,17 @@ public:
         {
             __values[key] = default_v;
         }
+        last_accessed_key = key;
         return __values[key];
     }
-    
+
     using iterator = decltype(std::map<int, T>().cbegin());
     iterator begin() const { return this->__values.cbegin(); }
     iterator end() const { return this->__values.cend(); }
 
 private:
     std::map<int, T> __values;
+    int              last_accessed_key;
 };
 
 
@@ -105,13 +133,17 @@ public:
 
     Matrix()            = default;
     ~Matrix()           = default;
-    
-    Layer operator[] (const int& key) { return layers[key]; }
+
+    Layer operator[] (const int& key)
+    {
+        layers.clean ( );
+        return layers[key];
+    }
 
     struct iterator : public std::iterator<std::forward_iterator_tag, IterPtr> {
     public:
         IterPtr     it;
-        
+
         iterator(const MatrixMap* layers)
         {
            for ( auto key_layer_pair: *layers )
@@ -120,48 +152,22 @@ public:
            }
            this->__values.push_back(__stub);
            __move_to_begin();
-        }
-       
-        template<typename Tuple>
-        void
-        __insert_values_to_iterator (Tuple tuple, std::pair<const int, value_type> key_layer_pair)
-        {
-            this->__values.push_back(std::tuple_cat(tuple, key_layer_pair));
-        }
-        
-        template <typename Tuple, typename Pair>
-        void
-        __insert_values_to_iterator(Tuple tuple, Pair key_layer_pair)
-        {
-            for ( auto __key_layer_pair: key_layer_pair.second )
-            {
-                __insert_values_to_iterator(
-                        std::tuple_cat(
-                            tuple, std::tuple<const int>(key_layer_pair.first)), __key_layer_pair);
-            }
-        }
-       
-        template <size_t ...Ns>
-        auto
-        __compile_tuple( std::index_sequence<Ns...>, value_type __default_v )
-        {
-            return std::tuple_cat(
-                    std::make_tuple((__stub_value(Ns))...), std::tuple<value_type>(__default_v));
+           std::cout << "Iterator size: " << __values.size ( ) << std::endl;
         }
 
         IterValue
         __make_stub()
         {
-            return __compile_tuple( std::make_index_sequence<dimensions>{}, default_v );
+            return __compile_tuple( std::make_index_sequence<dimensions> { }, default_v );
         }
-        
+
         void
         __move_to_begin()
         {
             this->it = this->__values.begin();
         }
 
-        void 
+        void
         __move_to_end()
         {
             this->it = this->__values.end();
@@ -175,7 +181,7 @@ public:
         bool operator== (iterator iter)     const   { return *it == *iter.it; }
 
         iterator& operator++ () {
-            do 
+            do
             {
                 ++this->it;
             }
@@ -186,13 +192,47 @@ public:
 
         const IterValue&    operator*()     const   { return (const IterValue&) *(this->it); }
         const IterValue*    operator->()    const   { return (const IterValue&) &*(this->it); }
-    
+
     private:
         std::list<IterValue>    __values;
         IterValue               __stub = __make_stub();
+
+        const int
+        __stub_value ( size_t )
+        {
+            return -1;
+        }
+
+        template <size_t ...Ns>
+        auto
+        __compile_tuple( std::index_sequence<Ns...>, value_type __default_v )
+        {
+            return std::tuple_cat(
+                    std::make_tuple((__stub_value( Ns ))...), std::tuple<value_type>(__default_v));
+        }
+
+        template<typename Tuple>
+        void
+        __insert_values_to_iterator (Tuple tuple, std::pair<const int, value_type> key_layer_pair)
+        {
+            this->__values.push_back(std::tuple_cat(tuple, key_layer_pair));
+        }
+
+        template <typename Tuple, typename Pair>
+        void
+        __insert_values_to_iterator(Tuple tuple, Pair key_layer_pair)
+        {
+            for ( auto __key_layer_pair: key_layer_pair.second )
+            {
+                __insert_values_to_iterator(
+                        std::tuple_cat(
+                            tuple, std::tuple<const int>(key_layer_pair.first)), __key_layer_pair);
+            }
+        }
+
     };
 
-    iterator begin()    const 
+    iterator begin()    const
     {
         iterator iter(&this->layers);
         while ( std::get<dimensions>(*iter) == default_v && *iter != iter.__make_stub() )
@@ -203,22 +243,22 @@ public:
     }
 
     iterator
-    end()      const 
-    { 
+    end()      const
+    {
         iterator iter(&this->layers);
         iter.__move_to_end();
         return iter;
     }
 
-    size_t size() { 
+    size_t size() {
         size_t __size = 0;
-        for ( auto it = this->begin(); it != this->end(); ++it )
+        for ( auto it: *this )
         {
             ++__size;
         }
         return __size;
     }
-    
+
 private:
-    MatrixMap layers;
+    MatrixMap   layers;
 };
